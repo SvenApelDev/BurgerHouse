27bdb082dd32c1e7d0a27b2299762e0c584773ce
Sven Apel <svap1201@gmail.com>
2026-08-03 07:55:28 +0200

    Extract fonts and data into separate files, add hover states

--- script.js
+++ script.js
@@ -1,121 +1,5 @@
-// ------ PRODUCTS DATA ------
-
-let products = [
-	{
-		name: "Veggie mushroom black burger",
-		description:
-			"Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-		price: 16.9,
-		imageMobile: "assets/images/veggie-burger.png",
-		imageDesktop: "assets/images/veggie-burger-desktop.png",
-		category: "burger",
-	},
-	{
-		name: "All meat burger",
-		description:
-			"Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-		price: 15.9,
-		imageMobile: "assets/images/meat-burger.png",
-		imageDesktop: "assets/images/meat-burger-desktop.png",
-		category: "burger",
-	},
-	{
-		name: "Beef red burger",
-		description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
-		price: 14.9,
-		imageMobile: "assets/images/beef-burger.png",
-		imageDesktop: "assets/images/beef-burger-desktop.png",
-		category: "burger",
-	},
-	{
-		name: "Big chicken burger",
-		description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-		price: 15.9,
-		imageMobile: "assets/images/chicken-burger.png",
-		imageDesktop: "assets/images/chicken-burger-desktop.png",
-		category: "burger",
-	},
-	{
-		name: "Pizza Margherita",
-		description: "Tomato Sauce, Mozzarella",
-		price: 11.9,
-		imageMobile: "assets/images/margherita-pizza.png",
-		imageDesktop: "assets/images/margherita-pizza-desktop.png",
-		category: "pizza",
-	},
-	{
-		name: "Pizza Chorizo",
-		description: "Tomato slices, Mozzarella, Chorizo",
-		price: 13.9,
-		imageMobile: "assets/images/chorizo-pizza.png",
-		imageDesktop: "assets/images/chorizo-pizza-desktop.png",
-		category: "pizza",
-	},
-	{
-		name: "Pizza Funghi",
-		description: "Red onion, Olives, Button Mushrooms, Mozzarella",
-		price: 12.9,
-		imageMobile: "assets/images/funghi-pizza.png",
-		imageDesktop: "assets/images/funghi-pizza-desktop.png",
-		category: "pizza",
-	},
-	{
-		name: "Quattro Formaggi with Chicken",
-		description:
-			"Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-		price: 16.9,
-		imageMobile: "assets/images/quattro-pizza.png",
-		imageDesktop: "assets/images/quattro-pizza-desktop.png",
-		category: "pizza",
-	},
-	{
-		name: "Warm beef arugula salad",
-		description:
-			"Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-		price: 16.9,
-		imageMobile: "assets/images/beef-arugula-salad.png",
-		imageDesktop: "assets/images/beef-arugula-salad-desktop.png",
-		category: "salad",
-	},
-	{
-		name: "Mini green Salad",
-		description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
-		price: 7.9,
-		imageMobile: "assets/images/mini-green-salad.png",
-		imageDesktop: "assets/images/mini-green-salad-desktop.png",
-		category: "salad",
-	},
-	{
-		name: "Green Salad with sea food",
-		description:
-			"Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-		price: 16.9,
-		imageMobile: "assets/images/green-sea-food-salad.png",
-		imageDesktop: "assets/images/green-sea-food-salad-desktop.png",
-		category: "salad",
-	},
-	{
-		name: "Vegan green salad with tofu",
-		description:
-			"Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-		price: 14.9,
-		imageMobile: "assets/images/vegan-green-salad.png",
-		imageDesktop: "assets/images/vegan-green-salad-desktop.png",
-		category: "salad",
-	},
-];
-
-// ------ BASKET ------
 
 let basket = [];
-
-// ------ CATEGORIES ------
-
-const categories = [
-	{ name: "burger", ref: "list-burger" },
-	{ name: "pizza", ref: "list-pizza" },
-	{ name: "salad", ref: "list-salad" },
-];
 
 // ------ INIT ------
 
@@ -330,6 +214,13 @@ function renderBadge() {
 		count = count + basket[i].amount;
 	}
 	document.getElementById("basket-badge").textContent = count;
+
+	const basketIcon = document.getElementById("basket-icon");
+	if (basket.length > 0) {
+		basketIcon.src = "assets/icons/ic-basket-active.svg";
+	} else {
+		basketIcon.src = "assets/icons/ic-basket.svg";
+	}
 }
 
 function closeConfirmation() {
